e41231544faa882129cf770a459323f189e17cc8
LeHaine <colt.daily@gmail.com>
2021-03-19 13:21:09 -0400

    init project setup with ldtk api

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.soywiz.korge")
+    kotlin("kapt") version "1.4.31"
+}
+
+korge {
+    targetJvm()
+    targetJs()
+}
+
+repositories {
+    maven("https://jitpack.io")
+}
+val ldtkApiVersion: String by project
+
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("com.lehaine.kt-ldtk-api:ldtk-api:$ldtkApiVersion")
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                configurations.all { // kapt has an issue with determining the correct KMM library, so we need to help it
+                    if (name.contains("kapt")) {
+                        attributes.attribute(
+                            org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.attribute,
+                            org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.jvm // pass in the JVM
+                        )
+                        attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage::class.java, Usage.JAVA_RUNTIME))
+                    }
+                }
+                configurations["kapt"].dependencies.add(project.dependencies.create("com.lehaine.kt-ldtk-api:ldtk-processor:$ldtkApiVersion"))
+            }
+        }
+    }
+}
