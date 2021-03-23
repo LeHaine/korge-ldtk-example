aa353b37349a0a72f8b079be187b6bb3e8bfbf6b
LeHaine <colt.daily@gmail.com>
2021-03-23 18:49:18 -0400

    add art workflow with shell scripts
    add some tiles and test hero animations
    
    add test hero to level scene

--- src/commonMain/kotlin/com/lehaine/ldtkbase/LevelScene.kt
+++ src/commonMain/kotlin/com/lehaine/ldtkbase/LevelScene.kt
@@ -8,19 +8,36 @@ import com.soywiz.korev.Key
 import com.soywiz.korge.input.keys
 import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.view.*
+import com.soywiz.korim.atlas.readAtlas
 import com.soywiz.korio.async.launchImmediately
+import com.soywiz.korio.file.std.resourcesVfs
+import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
 class LevelScene(val world: World, val levelIdx: Int = 0) : Scene() {
 
     override suspend fun Container.sceneInit() {
-        val level = world.allLevels[levelIdx].toLDtkLevel()
+        val worldLevel = world.allLevels[levelIdx]
+        val ldtkLevel = worldLevel.toLDtkLevel()
+        val atlas = resourcesVfs["tiles.atlas.json"].readAtlas()
+
+        val heroIdle = atlas.getSpriteAnimation("heroIdle", defaultTimePerFrame = 450.milliseconds)
+        val heroRun = atlas.getSpriteAnimation("heroRun", defaultTimePerFrame = 150.milliseconds)
+
 
         container {
-            val camera = camera {
-                ldtkMapView(level, renderIntGridLayer = true, debugEntities = true)
-            }
+            val playerData = worldLevel.layerEntities.allPlayer[0]
+            ldtkMapView(ldtkLevel)
+            val hero = Sprite(heroIdle).apply {
+                smoothing = false
+                anchor(playerData.pivotX, playerData.pivotY)
+                xy(
+                    playerData.pixelX,
+                    playerData.pixelY
+                )
+                playAnimationLooped()
+            }.addTo(this)
 
             val fpsText = text("FPS: ...")
                 .apply { smoothing = false }
@@ -48,20 +65,33 @@ class LevelScene(val world: World, val levelIdx: Int = 0) : Scene() {
                 }
 
             }
+            val speed = 0.1
             addUpdater { dt ->
                 val scale = if (dt == 0.milliseconds) 0.0 else (dt / 16.666666.milliseconds)
-                if (views.input.keys[Key.D]) dx -= 1.0
-                if (views.input.keys[Key.A]) dx += 1.0
-                if (views.input.keys[Key.W]) dy += 1.0
-                if (views.input.keys[Key.S]) dy -= 1.0
+                if (views.input.keys[Key.D]) {
+                    dx += speed
+                    hero.scaleX = 1.0
+                }
+                if (views.input.keys[Key.A]) {
+                    dx -= speed
+                    hero.scaleX = -1.0
+                }
                 if (views.input.keys[Key.ESCAPE]) stage?.gameWindow?.close()
 
-                dx = dx.clamp(-10.0, +10.0)
-                dy = dy.clamp(-10.0, +10.0)
-                camera.x += dx * scale
-                camera.y += dy * scale
+                dx = dx.clamp(-1.5, +1.5)
+                dy = dy.clamp(-1.5, +1.5)
+                hero.x += dx * scale
                 dx *= 0.9.pow(scale)
                 dy *= 0.9.pow(scale)
+                if (abs(dx) <= 0.08) {
+                    dx = 0.0
+                }
+
+                if (dx == 0.0) {
+                    hero.playAnimationLooped(heroIdle)
+                } else {
+                    hero.playAnimationLooped(heroRun)
+                }
 
                 accumulator += dt.millisecondsInt
                 if (accumulator > 200) {
